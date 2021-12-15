c2d852ce7e0c5924407596ca291d8c34fca722a6
Nathannicolle <nathan-nicolle@orange.fr>
2021-12-15 23:47:00 +0100

    modification du logo au scroll + modifs de style
    le logo devient plus épuré au scroll pour prendre moins d'espace

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,11 @@
-window.addEventListener("scroll", () => {
-    if (window.scrollY > document.querySelector(".banner").clientHeight) {
-        document.querySelector("img").src = "Proxmox pannel V7.2_light.png";
-        document.querySelector("img").style = "width: 100px; height: auto;";
-    } else {
-        document.querySelector("img").src = "Proxmox pannel V7.2_full.png";
-        document.querySelector("img").style = "width: 300px; height: auto;";
-    }
+window.addEventListener("load", () => {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > document.querySelector(".banner").clientHeight) {
+            document.querySelector(".logo").style = "height: 100px;";
+            document.querySelector(".logo").src = "assets/img/Proxmox_pannel_V7.2_light.png";
+        } else {
+            document.querySelector(".logo").src = "assets/img/Proxmox_pannel_V7.2_full.png";
+            document.querySelector(".logo").style = "height: 200px;";
+        }
+    });
 });
