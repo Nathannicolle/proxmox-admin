75fb003b0e1b52381c4f64ca0a5367460d31dc72
Nathannicolle <nathan-nicolle@orange.fr>
2021-12-20 21:57:14 +0100

    fix du pb de header
    le lien vers la page de connexion n'appairaissait pas

--- app/controllers/MyAuth.php
+++ app/controllers/MyAuth.php
@@ -1,42 +1,32 @@
 <?php
 namespace controllers;
+use Ubiquity\attributes\items\router\Post;
 use Ubiquity\attributes\items\router\Route;
+use Ubiquity\utils\flash\FlashMessage;
 use Ubiquity\utils\http\UResponse;
 use Ubiquity\utils\http\USession;
 use Ubiquity\utils\http\URequest;
 
 #[Route(path: "/login",inherited: true,automated: true)]
-class MyAuth extends \Ubiquity\controllers\auth\AuthController{
+class MyAuth extends \Ubiquity\controllers\auth\AuthController {
+    protected $headerView = "@activeTheme/main/vHeader.html";
 
-    protected function onConnect($connected) {
-        $urlParts=$this->getOriginalURL();
-        USession::set($this->_getUserSessionKey(), $connected);
-        if(isset($urlParts)){
-            $this->_forward(implode("/",$urlParts));
-        }else{
-            UResponse::header('Location', '/main');
-        }
+    protected $footerView = "@activeTheme/main/vFooter.html";
+
+    #[Route(path: "/signin", name: "auth.login")]
+    public  function getConnectFrom() {
+        $this->loadView("MyAuth/index.html");
     }
 
-    protected function _connect() {
-        if(URequest::isPost()){
-            $role=URequest::post($this->_getLoginInputName());
-            $password=URequest::post($this->_getPasswordInputName());
-            return $role;
-        }
-        return;
+    protected function terminateMessage(FlashMessage $fMessage)
+    {
+        $fMessage->setTitle('Déconnexion');
+        $fMessage->setContent('Déconnexion correctement effectuée !');
+        $fMessage->setIcon('warning');
     }
 
-    /**
-     * {@inheritDoc}
-     * @see \Ubiquity\controllers\auth\AuthController::isValidUser()
-     */
-    public function _isValidUser($action=null) {
-        return USession::exists($this->_getUserSessionKey());
-    }
-
-    public function _getBaseRoute() {
-        return '/login';
+    public function _displayInfoAsString() {
+        return true;
     }
 
     protected function initializeAuth()
@@ -51,6 +41,50 @@ class MyAuth extends \Ubiquity\controllers\auth\AuthController{
         if (! URequest::isAjax()) {
             $this->loadView($this->footerView);
         }
+    }
+
+    protected function noAccessMessage(FlashMessage $fMessage){
+        $fMessage->setTitle('Accès refusé !');
+        $fMessage->setContent('Vous n\'êtes pas authentifié');
+        $fMessage->setIcon('warning sign');
+    }
+
+
+    protected function onConnect($connected) {
+        $urlParts=$this->getOriginalURL();
+        USession::set($this->_getUserSessionKey(), $connected);
+        if(isset($urlParts)){
+            $this->_forward(implode("/",$urlParts));
+        }else{
+            //TODO
+            UResponse::header('location', '/');
+        }
+    }
+
+    #[Post(path:"/connect/", name:"myAuth.connect")]
+    protected function _connect() {
+        if(URequest::isPost()){
+            $email=URequest::post($this->_getLoginInputName());
+            $password=URequest::post($this->_getPasswordInputName());
+            $user=DAO::getOne(User::class,'email= :email',false,['email'=>$email]);
+            if(isset($user)) {
+                USession::set('idOrga', $user->getOrganization());
+            }
+            return $user;
+        }
+        return ;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Ubiquity\controllers\auth\AuthController::isValidUser()
+     */
+    public function _isValidUser($action=null) {
+        return USession::exists($this->_getUserSessionKey());
+    }
+
+    public function _getBaseRoute() {
+        return '/login';
     }
 	
 
