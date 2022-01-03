07cf7f46e3ba2ccd0799e620dcdbb9e16d7a7cbc
Nathannicolle <nathan-nicolle@orange.fr>
2022-01-03 17:03:52 +0100

    ajout d'un message d'accueil variable (dashboard)

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -1,7 +1,8 @@
 <?php
 namespace controllers;
 use Ubiquity\controllers\admin\UbiquityMyAdminBaseController;
+use Ubiquity\controllers\auth\AuthController;
+use Ubiquity\controllers\auth\WithAuthTrait;
 
 class Admin extends UbiquityMyAdminBaseController{
-
 }
