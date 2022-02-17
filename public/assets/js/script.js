aab893de2c3b5572ef18d2ccacdb9819ccf83da6
Nathannicolle <nathan-nicolle@orange.fr>
2022-02-17 11:22:37 +0100

    style, reponsive et optimisations
    utilisation de semantic pour les boutons (dashboard) + nouveau lien de déconnexion

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -122,13 +122,26 @@ window.addEventListener("load", () => {
             }
         });
 
+        // reduced menu on certain width
+        if(window.innerWidth <= 900) {
+            $(".minimize_menu i").removeClass("fa-caret-square-left");
+            $(".minimize_menu i").addClass("fa-caret-square-right");
+            document.querySelector("aside.dashboard_menu").style = "width: 5vw !important;";
+            document.querySelector("aside.dashboard_menu ul").style = "margin-left: 0 !important; padding-left: 0 !important;";
+            document.querySelectorAll("aside.dashboard_menu li a").forEach(element => {
+                element.querySelector("span").style = "display: none !important;";
+                element.querySelector("i").style = "font-size: 1.7em;";
+                element.style = "margin-left: auto; margin-bottom: 7%; padding-right: 40%;";
+            });
+        }
+
         // minimize menu
         $(".minimize_menu").click(function () {
             if(document.querySelector(".minimize_menu i").classList.contains("fa-caret-square-left")) {
                 $(".minimize_menu i").removeClass("fa-caret-square-left");
                 $(".minimize_menu i").addClass("fa-caret-square-right");
                 document.querySelector("aside.dashboard_menu").style = "width: 5vw !important;";
-                document.querySelector("aside.dashboard_menu ul").style = "margin-left: 0;";
+                document.querySelector("aside.dashboard_menu ul").style = "margin-left: 0 !important; padding-left: 0 !important;";
                 document.querySelectorAll("aside.dashboard_menu li a").forEach(element => {
                     element.querySelector("span").style = "display: none !important;";
                     element.querySelector("i").style = "font-size: 1.7em;";
@@ -139,6 +152,9 @@ window.addEventListener("load", () => {
                 $(".minimize_menu i").addClass("fa-caret-square-left");
                 document.querySelector("aside.dashboard_menu span").style = "display: inline !important;";
                 document.querySelector("aside.dashboard_menu").style = "width: 12vw !important;";
+                if(window.innerWidth >= 1300) {
+                    document.querySelector("aside.dashboard_menu ul").style = "padding-left: 40px !important;";
+                }
                 document.querySelectorAll("aside.dashboard_menu li a").forEach(element => {
                     element.querySelector("span").style = "display: inline !important;";
                     element.querySelector("i").style = "font-size: 1em;";
