b0b24ca32e3d29c415b294b26426365b9b09294e
Nathannicolle <nathan-nicolle@orange.fr>
2022-03-12 23:44:40 +0100

    new responsive fullscreen menu V1

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -149,6 +149,29 @@ window.addEventListener("load", () => {
     } else {
         document.querySelector(".logo").style = "height: 100px;";
         document.querySelector(".logo").src = "assets/img/Proxmox_pannel_V7.2_light.png";
+
+        /* Fullscreen menu animations */
+        document.querySelectorAll(".fullscreen_menu .menu a").forEach(element => {
+            element.addEventListener("mouseover", () => {
+                document.querySelector(".wrapper_" + element.getAttribute("id") + " img").style = "opacity: 1; transform: scale(1.2);";
+            });
+
+            element.addEventListener("mouseleave", () => {
+                document.querySelector(".wrapper_" + element.getAttribute("id") + " img").style = "opacity: 0.4; transform: scale(1);";
+            });
+        });
+
+        document.querySelectorAll(".wrapper").forEach(element => {
+            let splited_classes = element.getAttribute("class").split(" ");
+            let splitted_class_name = splited_classes[1].split("_");
+            element.addEventListener("mouseover", () => {
+                document.getElementById(splitted_class_name[1]).style = "color: white;";
+            });
+
+            element.addEventListener("mouseleave", () => {
+                document.getElementById(splitted_class_name[1]).style = "color: gray;";
+            });
+        });
     }
 
     if(typeof document.querySelector("div.container_404") !== 'undefined') {
