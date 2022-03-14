1f1fe2e4d61ec86b141b682d2410e245b1ca9558
Nathannicolle <nathan-nicolle@orange.fr>
2022-03-15 00:30:40 +0100

    factorisation du js lié au fullscreen menu
    afin de réduire le code, d'éviter les doublons et de simplifier le code

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,38 @@
 window.addEventListener("load", () => {
+    // -- Fullscreen menu animations and grid disposition -- //
+    function fullscreenMenu() {
+        if(window.innerWidth >= 900) {
+            if (document.querySelectorAll(".fullscreen_menu .menu a").length === 3) {
+                document.querySelector(".fullscreen_menu .menu").style = "grid-template-columns: repeat(3, 20%) !important;";
+            } else if (document.querySelectorAll(".fullscreen_menu .menu a").length === 2) {
+                document.querySelector(".fullscreen_menu .menu").style = "grid-template-columns: repeat(2, 20%) !important;";
+            }
+        }
+
+        document.querySelectorAll(".fullscreen_menu .menu a").forEach(element => {
+            element.addEventListener("mouseover", () => {
+                document.querySelector(".wrapper_" + element.getAttribute("id") + " img").style = "opacity: 1; transform: scale(1.2);";
+            });
+
+            element.addEventListener("mouseleave", () => {
+                document.querySelector(".wrapper_" + element.getAttribute("id") + " img").style = "opacity: 0.4; transform: scale(1);";
+            });
+        });
+
+
+        document.querySelectorAll(".wrapper").forEach(element => {
+            let splited_classes = element.getAttribute("class").split(" ");
+            let splitted_class_name = splited_classes[1].split("_");
+            element.addEventListener("mouseover", () => {
+                document.getElementById(splitted_class_name[1]).style = "color: white;";
+            });
+
+            element.addEventListener("mouseleave", () => {
+                document.getElementById(splitted_class_name[1]).style = "color: gray;";
+            });
+        });
+    }
+
     if(window.location.pathname === "/") {
         if(window.innerWidth <= 1300) {
             document.querySelector(".logo").style = "height: 100px;";
@@ -67,38 +101,10 @@ window.addEventListener("load", () => {
 
         document.querySelector("footer").style.display = "block";
 
-        /* Fullscreen menu animations */
-        if(window.innerWidth >= 900) {
-            if (document.querySelectorAll(".fullscreen_menu .menu a").length === 3) {
-                document.querySelector(".fullscreen_menu .menu").style = "grid-template-columns: repeat(3, 20%) !important;";
-            } else if (document.querySelectorAll(".fullscreen_menu .menu a").length === 2) {
-                document.querySelector(".fullscreen_menu .menu").style = "grid-template-columns: repeat(2, 20%) !important;";
-            }
-        }
+        // -- Fullscreen menu animations and grid disposition -- //
+        fullscreenMenu();
 
-        document.querySelectorAll(".fullscreen_menu .menu a").forEach(element => {
-            element.addEventListener("mouseover", () => {
-                document.querySelector(".wrapper_" + element.getAttribute("id") + " img").style = "opacity: 1; transform: scale(1.2);";
-            });
-
-            element.addEventListener("mouseleave", () => {
-                document.querySelector(".wrapper_" + element.getAttribute("id") + " img").style = "opacity: 0.4; transform: scale(1);";
-            });
-        });
-
-
-        document.querySelectorAll(".wrapper").forEach(element => {
-            let splited_classes = element.getAttribute("class").split(" ");
-            let splitted_class_name = splited_classes[1].split("_");
-            element.addEventListener("mouseover", () => {
-                document.getElementById(splitted_class_name[1]).style = "color: white;";
-            });
-
-            element.addEventListener("mouseleave", () => {
-                document.getElementById(splitted_class_name[1]).style = "color: gray;";
-            });
-        });
-    } else if(window.location.pathname.match("/dashboard") || window.location.pathname.match("/dashboard_[a-zA-Z]{0,8}") ||  window.location.pathname.match("/oneVM") || window.location.pathname.match("/oneServer") || window.location.pathname.match('/createForm') || window.location.pathname.match('/modifyForm') || window.location.pathname.match('/groupeModifyForm') | window.location.pathname.match('/user/droitForm/{0,9}/')) {
+    } else if(window.location.pathname.match("/dashboard") || window.location.pathname.match("/dashboard_[a-zA-Z]{0,8}") ||  window.location.pathname.match("/oneVM") || window.location.pathname.match("/oneServer") || window.location.pathname.match('/createForm') || window.location.pathname.match('/modifyForm') || window.location.pathname.match('/groupeModifyForm') || window.location.pathname.match('/user/[a-zA-Z]{0,10}')) {
         document.querySelector(".ui.basic.inverted.segment.main_menu").style = "display: none !important;";
         document.querySelector(".page_container").style = "margin-top: 0; padding-bottom:0;";
         $(".subcategory").click(function () {
@@ -203,34 +209,8 @@ window.addEventListener("load", () => {
         document.querySelector(".logo").style = "height: 100px;";
         document.querySelector(".logo").src = "assets/img/Proxmox_pannel_V7.2_light.png";
 
-        /* Fullscreen menu animations */
-        if(document.querySelectorAll(".fullscreen_menu .menu a").length === 3) {
-            document.querySelector(".fullscreen_menu .menu").style = "grid-template-columns: repeat(3, 20%) !important;";
-        } else if (document.querySelectorAll(".fullscreen_menu .menu a").length === 2) {
-            document.querySelector(".fullscreen_menu .menu").style = "grid-template-columns: repeat(2, 20%) !important;";
-        }
-
-        document.querySelectorAll(".fullscreen_menu .menu a").forEach(element => {
-            element.addEventListener("mouseover", () => {
-                document.querySelector(".wrapper_" + element.getAttribute("id") + " img").style = "opacity: 1; transform: scale(1.2);";
-            });
-
-            element.addEventListener("mouseleave", () => {
-                document.querySelector(".wrapper_" + element.getAttribute("id") + " img").style = "opacity: 0.4; transform: scale(1);";
-            });
-        });
-
-        document.querySelectorAll(".wrapper").forEach(element => {
-            let splited_classes = element.getAttribute("class").split(" ");
-            let splitted_class_name = splited_classes[1].split("_");
-            element.addEventListener("mouseover", () => {
-                document.getElementById(splitted_class_name[1]).style = "color: white;";
-            });
-
-            element.addEventListener("mouseleave", () => {
-                document.getElementById(splitted_class_name[1]).style = "color: gray;";
-            });
-        });
+        // -- Fullscreen menu animations and grid disposition -- //
+        fullscreenMenu();
     }
 
     if(typeof document.querySelector("div.container_404") !== 'undefined') {
@@ -253,11 +233,9 @@ window.addEventListener("load", () => {
             if(event.key === "ArrowUp" || event.key === "Up" || event.key === "KeyZ") {
                 document.querySelector("i.fighter.jet.icon").classList.remove("translate_down");
                 document.querySelector("i.fighter.jet.icon").classList.add("translate_up");
-                console.log("up");
             } else if (event.key === "ArrowDown" || event.key === "Down" || event.key === "KeyS") {
                 document.querySelector("i.fighter.jet.icon").classList.remove("translate_up");
                 document.querySelector("i.fighter.jet.icon").classList.add("translate_down");
-                console.log("down");
             } else if (event.key === " ") {
                 if(plane.style.animationPlayState === 'running') {
                     plane.style.animationPlayState = 'paused';
