3e3a37926f6b7e9081ec635fd391b0e4fdf703f8
Nathannicolle <nathan-nicolle@orange.fr>
2022-03-15 11:53:07 +0100

    add config files

---
+++ app/config/config.php
@@ -0,0 +1,57 @@
+<?php
+return array(
+	"siteUrl"=>"http://127.0.0.1:8090/",
+	"database"=>[
+			"type"=>"mysql",
+			"dbName"=>"proxmox",
+			"serverName"=>"127.0.0.1",
+			"port"=>3306,
+			"user"=>"root",
+			"password"=>"  ",
+			"options"=>[],
+			"cache"=>false,
+			"wrapper"=>"Ubiquity\\db\\providers\\pdo\\PDOWrapper"
+			],
+	"sessionName"=>"s61b7bf5707551",
+	"namespaces"=>[],
+	"templateEngine"=>"Ubiquity\\views\\engine\\Twig",
+	"templateEngineOptions"=>[
+			"cache"=>false
+			],
+	"test"=>false,
+	"debug"=>true,
+	"logger"=>function (){
+        return new \Ubiquity\log\libraries\UMonolog(array (
+  'host' => '127.0.0.1',
+  'port' => 8090,
+  'sessionName' => 's61b7bf5707551',
+)['sessionName'], \Monolog\Logger::INFO);
+    },
+	"di"=>[
+			"@exec"=>[
+					"jquery"=>function ($controller){
+						return \Ajax\php\ubiquity\JsUtils::diSemantic($controller);
+					}
+					]
+			],
+	"cache"=>[
+			"directory"=>"cache/",
+			"system"=>"Ubiquity\\cache\\system\\ArrayCache",
+			"params"=>[]
+			],
+	"mvcNS"=>[
+			"models"=>"models",
+			"controllers"=>"controllers",
+			"rest"=>"",
+			"domains"=>"domains"
+			],
+	"onError"=>function ($code, $message = NULL, $controllerInstance = NULL){
+				switch ($code) {
+					case 404:
+					case 500:
+						throw new \Exception($message);
+						break;
+				}
+			},
+	"encryption-key"=>"68d9859fb01a4bba8751815a1a0369fc"
+	);
